0873b645e50f5d0f84e2f62ea9e1ebb3bb217c0e
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2025-05-17 16:26:07 -0300

    Agregado bucle de menu principal

--- main_toolkit.sh
+++ main_toolkit.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-echo "hola"
+# Bienvenida
+echo "Bienvenido al Sistema de Gestion el Palomar"
+
+# Definicion de Menu principal
+while True; do
+
+done
+
+
 #chmod +x main_toolkit.sh 
 #./main_toolkit.sh 
