db6872473c682a8cb8719388924d285c17f89e6b
Lis <168233558+marialis19@users.noreply.github.com>
2025-06-06 22:57:49 -0300

    Integro submenú de disco al main_toolkit.sh

--- main_toolkit.sh
+++ main_toolkit.sh
@@ -28,19 +28,26 @@ submenu_memoria() {
   sh ./memoria.sh # ejecuta memoria.sh que ya tiene su propio read
 }
 
+# funcion para el submenu de disco
+submenu_disco() {
+  sh ./disco.sh
+}
+
 # menu principal
 while true; do
   echo "========= MENÚ PRINCIPAL ========="
   echo "1. Opción acerca de procesos"
   echo "2. Opción acerca de memoria"
-  echo "3. Salir"
+  echo "3. Opción acerca de disco"
+  echo "4. Salir"
   echo "=================================="
   read -p "Seleccione una opción: " opcion # variable opcion
 
   case $opcion in
-    1) submenu_procesos;; # llama a la funcion submenu_procesos 
+    1) submenu_procesos;;
     2) submenu_memoria;;
-    3) echo "Saliendo..."; exit 0;; # exit para salir, el 0 es porque termino correctamente sin errores
+    3) submenu_disco;;
+    4) echo "Saliendo..."; exit 0;;
     *) echo "Opción inválida."; read -p "Presione Enter para intentar de nuevo...";;
   esac
 done
